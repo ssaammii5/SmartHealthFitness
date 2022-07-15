26ae1e4d32dce1fc886764020661775bae49c8b7
Sami <47335566+ssaammii5@users.noreply.github.com>
2022-07-16 04:38:45 +0600

    Initial commit

---
+++ app/src/main/java/com/health/smarthealthfitness/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.health.smarthealthfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Animation up,down;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        ImageView imageView = findViewById(R.id.appsplash);
+        up = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.up);
+        imageView.setAnimation(up);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },3500);
+    }
+}
